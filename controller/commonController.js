96008bff47cee273b52028a381c18f8e9443cc18
Adarshkiraula <adarshkirola5@gmail.com>
2020-03-03 05:16:18 +0530

    first

--- controller/commonController.js
+++ controller/commonController.js
@@ -1,40 +1,51 @@
 const model = require('../model/user');
 const key =require('../key');
-// const jwt =require('jsonwebtoken');
+const jwt =require('jsonwebtoken');
 const nodemailer = require("nodemailer");
 var generator = require('generate-password');
 
 //const sha1 = require('sha1');
 var msg = "";
+var object_id;
+
 
  module.exports = {
-     login,registeruser,registersubadmin
-     //register
+     login,registeruser,registersubadmin,checkauth,userview,subadminview,del,mod,modify
+     //register,checkType
  }
-
-let transporter=nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:'adarshkirola5@gmail.com',
-        password:'adarshm1280'
+ let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'munjal.chirag.test@gmail.com',
+        pass: '7500598665'
     }
 });
-function tmail(email,password)
-{
-    let mailOptions={
-        from:'adarshkirola5@gmail.com',
-        to:'munjalchirag7500@gmail.com',
-        subject:'Thanku for register',
-        html:'your email is'+email +'your password is' +password
-    }
-    transporter.sendMail(mailOptions,function(err,info)
-    {
-        if(err)
-        console.log(err);
-        else
-        console.log(info);
-    })
+function eMail(email, password, req, res) {
+
+
+    let mailOptions = {
+        from: 'munjal.chirag.test@gmail.com',
+        to: 'abc@gmail.com',
+        subject: "Registration",
+        html: "Thanking You For Registering Your Email: " + email + "  Password: " + password
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+
+        if (error) {
+            console.log(error);
+
+        } else {
+            console.log('Email sent: ' + info.response);
+
+        }
+
+    });
+
+
 }
+
+
 // function register(req, res) {
 //     let name=req.body.name;
 //     let age=req.body.age;
@@ -88,9 +99,23 @@ function login(req,res)
             data.comparePassword(pass, function (err, isMatch) {
                 if (err) throw err;
                 console.log("Password Verified", isMatch);
-                res.render('index.html'); // -&gt; Password123: true
+                //res.render('index.html'); // -&gt; Password123: true
                 if (isMatch) {
-                    
+                    console.log(data)
+                    object_id=data.id;
+                    role=data.role;
+                    if(role=='admin'){
+                        console.log(object_id);
+                        let token=generateToken(object_id);
+                        console.log("Received Token",token);
+                        res.cookie('name',token).render('index.html');
+                    }
+                    else if(role=='subadmin'){
+                        res.json("subadmin");
+                    }
+                    else{
+                        res.json('user');
+                    }
                 }
 
                 else {
@@ -132,9 +157,16 @@ function registersubadmin(req,res)
         }
         else {
              msg = "Registered Succesfully"
-            
-             tmail(email,password);
-             console.log(data);
+            //token
+            console.log(data)
+            object_id=data.id;
+             console.log(object_id);
+             let token=generateToken(object_id);
+             console.log("Received Token",token);
+             res.cookie('name',token);
+            //
+             eMail(email,password);
+            // console.log(data);
             res.render('index.html');
         }
     })
@@ -161,11 +193,240 @@ function registeruser(req, res) {
         }
         else {
              msg = "Registered Succesfully"
+             console.log(data)
+             object_id=data.id;
+             console.log(object_id);
+             let token=generateToken(object_id);
+             console.log("Received Token",token);
+             res.cookie('name',token);
               
             // res.json("Registered");
-            console.log(data);
+           // console.log(data);
+           eMail(email,password);
             res.render('index.html');
         }
     })
 
 }
+function generateToken(value)
+{
+    console.log("Generate Id received" +value)
+    let token=jwt.sign({'id':value},key.secret,{expiresIn: '5m'});
+    return token;
+}
+function checkauth(req,res,next)
+{
+    let token=req.cookies.name;
+    console.log("Cookie"+token);
+    jwt.verify(token,key.secret,function(err)
+{
+    if(err)
+    {
+        console.log(err);
+        res.render('login.html');
+    }
+    else{
+        next();
+    }
+})
+}
+function userview(req, res) {
+    
+        model.find({ 'role': 'user'}, function (err, data) {
+            juser = data
+            res.render('userview.html', { juser });
+        })
+    
+    }
+function subadminview(req, res) {
+    
+        model.find({ 'role': 'subadmin'}, function (err, data) {
+            juser = data
+            res.render('subadminview.html', { juser });
+        })
+    
+    }
+    function mod(req,res){
+        let id = req.params.id;
+        console.log("*******"+id);
+        model.findOne({'_id':id},(err,data)=>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                
+                let name=data.name;
+                let age=data.age;
+                let email=data.email;
+                let role=data.role;
+                let id=data.id;
+                let is_deleted=data.is_deleted;
+                res.render('modify.html',{name,age,email,role,id,is_deleted});
+            }
+        })
+    }
+    function modify(req,res){
+        let name=req.body.name;
+        let age=req.body.age;
+        let role=req.body.role;
+        let id=req.body.id;
+        let is_deleted=req.body.is_deleted;
+        console.log(is_deleted)
+       model.findByIdAndUpdate({'_id':id},{$set:{'name':name,'age':age,'role':role,'is_deleted':is_deleted}},(err,data)=>{
+         if(err){
+             console.log(err)
+    
+         }  
+         else{
+             if(role=='subadmin'){
+              res.redirect('/subadminview');}
+              else{
+                   res.redirect('/userview');
+              }
+         }
+       })
+    
+    
+    }
+    function del(req,res)
+    {
+        let id = req.params.id;
+        console.log("#############"+id);
+        model.findOneAndUpdate({ '_id': id }, { $set: { 'is_deleted': 'true' } }, (err, data) => {
+            if (err) {
+                res.json("err")
+            }
+            else {
+                role = data.role;
+                
+                if (role == 'user') {
+                    res.redirect('/userview');
+                }
+                else {
+                    res.redirect('/subadminview');
+                }
+            }
+        })
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // function checkType(req, res, next) {
+    //     let type = req.type;
+    //     if (type == 'admin') {
+    //         next();
+    //     }
+    //     else {
+    //         msg = "Only Admin Is Authorized"
+    //          res.redirect('/');
+    //     }
+    
+    // }
--- routes/index.js
+++ routes/index.js
@@ -18,30 +18,43 @@ const commonController=require('../controller/commonController');
 
 
 /* GET Route */
-router.get('/',function(req,res){
+router.get('/',commonController.checkauth,function(req,res){
   res.render('login.html');
 });
 
 
 
- router.get('/adduser',function(req,res){
+ router.get('/adduser',commonController.checkauth,function(req,res){
   
   res.render('userregister.html');
  });
 
- router.get('/addsubadmin',function(req,res){
+ router.get('/addsubadmin',commonController.checkauth,function(req,res){
   
   res.render('subadminregister.html');
 });
 //router.post('/register',commonController.register);
-router.post('/registeruser',commonController.registeruser);
+router.post('/registeruser',commonController.checkauth,commonController.registeruser);
 
- router.post('/registersubadmin',commonController.registersubadmin);
+ router.post('/registersubadmin',commonController.checkauth,commonController.registersubadmin);
 
 router.post('/login',commonController.login);
 
 
+//
+router.get('/userview',commonController.checkauth,commonController.userview);
+ 
+router.get('/subadminview',commonController.checkauth,commonController.subadminview);
 
+router.get('/modify/:id',commonController.checkauth,commonController.mod)
+
+router.post('/update',commonController.checkauth,commonController.modify);
+
+router.get('/delete/:id',commonController.checkauth,commonController.del)
+
+router.get('/logout',(req,res)=>{
+  res.clearCookie('name').redirect('/');
+})
 
 
 
